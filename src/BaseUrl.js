d58b8949d7521c65b434e0a7cd190641a1e3e9f7
nguyen <nknguyen2112@gmail.com>
2022-05-07 22:46:09 +0700

    fix

--- src/BaseUrl.js
+++ src/BaseUrl.js
@@ -1,3 +1,3 @@
-const BASE_URL = process.env.NODE_ENV === "development" ? "http://localhost:3001" : "https://sheltered-retreat-14346.herokuapp.com";
+const BASE_URL = process.env.NODE_ENV === window.location.includes("localhost") ? "http://localhost:3001" : "https://sheltered-retreat-14346.herokuapp.com";
 
 export default BASE_URL;

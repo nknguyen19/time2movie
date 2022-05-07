9410ca5f09a0995c66db1178bfdff347953d4099
nguyen <nknguyen2112@gmail.com>
2022-05-07 22:31:31 +0700

    build folder

---
+++ build/precache-manifest.d1f78dbdd247ddaf09639219abf63335.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c4e1872cea29a1d18d9",
+    "url": "/static/css/main.b8ba56eb.chunk.css"
+  },
+  {
+    "revision": "3c4e1872cea29a1d18d9",
+    "url": "/static/js/main.3c4e1872.chunk.js"
+  },
+  {
+    "revision": "1facff382db8f62404dd",
+    "url": "/static/js/1.1facff38.chunk.js"
+  },
+  {
+    "revision": "a863b591fd5102e167ea",
+    "url": "/static/js/2.a863b591.chunk.js"
+  },
+  {
+    "revision": "cd9a9f06ca22a36be1ba",
+    "url": "/static/js/runtime~main.cd9a9f06.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "479bd5a5c52ddd5e9b6fe4b721010c6c",
+    "url": "/index.html"
+  }
+];
